881d565023494686caad53270249c50457a0c2d4
lhynjn9 <lhynjn9@gmail.com>
2022-04-06 04:03:19 +0900

    0404 commit

---
+++ 1일_1알고리즘/6603_로또.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('imput.txt')
+
+def comb(start):
+    if len(ans) == 6:
+        print(*ans)
+        return
+    for j in range(start, k):
+        if S[j] not in ans:
+            ans.append(S[j])
+            comb(j+1)
+            ans.pop()
+
+while True:
+    k, *S = map(int, input().split())
+    if k == 0:
+        break
+    else:
+        ans = []
+        comb(0)
+        print()
+
+dump = int(input())
+
+
