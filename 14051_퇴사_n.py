0cf49b84173ff501018034abcdbef2d38a396fd6
lhynjn9 <lhynjn9@gmail.com>
2022-04-20 21:48:01 +0900

    directory update

---
+++ 14051_퇴사_n.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin = open('imput.txt')
+
+def check(day, tot):
+    global ans
+    # 마지막 상담일 경우
+    if day + T[day] > N:
+        # 최대 이익 계산
+        if ans < tot:
+            ans = tot
+            return
+    elif day + T[day] == N:
+        tot += P[day]
+        if ans < tot:
+            ans = tot
+            return
+    # 마지막 상담이 아닐 경우
+    else:
+        check(day + T[day], tot+P[day]) # 현재 일에 상담을 할 경우
+        check(day+1, tot)# 현재 일에 상담을 하지 않을 경우
+
+N = int(input())
+T = [] # 상담완료 기간
+P = [] # 상담 금액
+ans = 0 # 출력값 : 최대 이익
+
+for i in range(N):
+    a, b = map(int, input().split())
+    T.append(a)
+    P.append(b)
+
+# 최대 이익을 구하기 위함 함수
+# 상담일, 상담 금액
+check(0,0)
+
+print(ans)
--- 15686_치킨배달.py
+++ 15686_치킨배달.py
@@ -1,21 +1,20 @@
-import sys
-sys.stdin = open('imput.txt')
-
 # 도시의 치킨 거리 최솟값 구하기
 def city_chick():
     total = 0
-    for m in range(len(house)):
+    for m in house:
+        r, c = m
         dump = 0  # 최솟값 비교를 위한 임시 값
         distance = 0xffffff  # 최소 거리
-        for n in range(len(visit)):
+        for n in visit:
             #  선택한 치킨집과 현재 집의 거리를 비교
             if visit[n] == 1:
-                dump = abs(house[m][0] - chick[n][0]) + abs(house[m][1] - chick[n][1])
+                dump = abs(r - chick[n][0]) + abs(c - chick[n][1])
                 if dump < distance:
                     distance = dump
         total += distance
     return total
 
+# 선택 개수, 선택 시작 좌표
 def pick(n, start):
     global min_ans
     if n == M: # M개의 치킨 집을 선택한 경우
@@ -24,8 +23,10 @@ def pick(n, start):
         if ans < min_ans:
             min_ans = ans
         return
+
     if start == len(visit):
         return
+
     else:
         # 치킨집 선택
         for k in range(start, len(visit)):
@@ -38,9 +39,9 @@ def pick(n, start):
 N, M = map(int, input().split())
 city  = [list(map(int, input().split())) for _ in range(N)]
 
-ans = 0
 chick = []
 house = []
+
 min_ans = 0xffffff
 
 # 치킨집과 집의 좌표 선택
@@ -53,7 +54,6 @@ for i in range(N):
             
 visit = [0] * len(chick)
 # 치킨 집 선택(조합)
-# 선택 개수, 선택 시작 좌표
 pick(0, 0)
 
 print(min_ans)
