e83164cbdaeb01d57c171030b3b02c4d2a90fe2c
lhynjn9 <lhynjn9@gmail.com>
2022-05-12 23:45:04 +0900

    220511 Create

--- 2292_벌집.py
+++ 2292_벌집.py
@@ -1,4 +1,11 @@
-import sys
-sys.stdin = open('sample.txt')
+'''
+분류 : 수학
+'''
+n = int(input())
 
-N = int(input())
+nums_pileup = 1  # 벌집의 개수, 1개부터 시작
+ans = 1
+while n > nums_pileup :
+    nums_pileup += 6 * ans  # 벌집이 6의 배수로 증가
+    ans += 1  # 반복문을 반복하는 횟수
+print(ans)
